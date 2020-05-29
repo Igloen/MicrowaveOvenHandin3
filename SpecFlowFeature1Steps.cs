92018d816c4591cab0d7fa83fb7249feac8b5c8a
Mads <au579023@uni.au.dk>
2020-05-29 15:04:38 +0200

    Virker ikke

---
+++ SpecFlowFeature1Steps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SystemTest
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"The oven is reset")]
+        public void GivenTheOvenIsReset()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press the time button (.*) time\(s\)")]
+        public void WhenIPressTheTimeButtonTimeS(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the display should show (.*) sec")]
+        public void ThenTheDisplayShouldShowSec(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ SystemTest/SpecFlowFeature1.feature
@@ -0,0 +1,8 @@
+﻿Feature: SpecFlowFeature1
+	Set the cooking time so I can heat my food
+
+@mytag
+Scenario: Set cooking time
+	Given The oven is reset
+	When I press the time button 1 time(s)
+	Then the display should show 20 sec
